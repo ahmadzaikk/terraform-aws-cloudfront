4e7d6e19f4221a8da4b7d7a96723fd3340b7f621
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-05 12:52:36 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,94 +1,16 @@
-variable "s3_bucket_prefix" {
-  description = "Prefix for the S3 bucket name."
+variable "origin_type" {
+  description = "The type of origin for the CloudFront distribution (s3 or alb)."
   type        = string
 }
 
-variable "viewer_protocol_policy" {
-  description = "The policy that determines which protocols are allowed for viewers."
-  type        = string
-  default     = "redirect-to-https"
-}
-
-variable "allowed_methods" {
-  description = "The HTTP methods that are allowed."
-  type        = list(string)
-  default     = ["GET", "HEAD"]
-}
-
-variable "cached_methods" {
-  description = "The HTTP methods that are cached."
-  type        = list(string)
-  default     = ["GET", "HEAD"]
-}
-
-variable "min_ttl" {
-  description = "The minimum amount of time, in seconds, that an object is cached."
-  type        = number
-  default     = 3600
-}
-
-variable "default_ttl" {
-  description = "The default amount of time, in seconds, that an object is cached."
-  type        = number
-  default     = 86400
-}
-
-variable "max_ttl" {
-  description = "The maximum amount of time, in seconds, that an object is cached."
-  type        = number
-  default     = 31536000
-}
-
-variable "forward_query_string" {
-  description = "Whether query strings are forwarded to the origin."
-  type        = bool
-  default     = false
-}
-
-variable "forward_headers" {
-  description = "The headers that are forwarded to the origin."
-  type        = list(string)
-  default     = ["Host", "Origin"]
-}
-
-variable "forward_cookies" {
-  description = "The type of cookies that are forwarded to the origin."
-  type        = string
-  default     = "none"
-}
-
-variable "acm_certificate_arn" {
-  description = "The ARN of the ACM certificate (leave empty if not using SSL/TLS)."
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket."
   type        = string
   default     = ""
 }
 
-variable "ssl_support_method" {
-  description = "The SSL/TLS support method."
+variable "alb_domain_name" {
+  description = "The domain name of the ALB (used if `origin_type` is `alb`)."
   type        = string
-  default     = "sni-only"
-}
-
-variable "price_class" {
-  description = "The price class for the distribution."
-  type        = string
-  default     = "PriceClass_100"
-}
-
-variable "enabled" {
-  description = "Whether the distribution is enabled."
-  type        = bool
-  default     = true
-}
-
-variable "origin_access_identity_comment" {
-  description = "Comment for the origin access identity."
-  type        = string
-  default     = "example-origin-access-identity"
-}
-
-variable "tags" {
-  description = "Tags to apply to all resources."
-  type        = map(string)
-  default     = {}
+  default     = ""
 }
