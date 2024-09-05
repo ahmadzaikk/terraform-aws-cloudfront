119635d592a11d08d6b4f388d385c99506812e46
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-05 12:55:07 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_id" {
-  value     = aws_s3_bucket.this.bucket
-  condition = var.origin_type == "s3"
+  value     = var.origin_type == "s3" ? aws_s3_bucket.this.bucket : null
+  sensitive = false
 }
 
 output "alb_domain_name" {
-  value     = var.alb_domain_name
-  condition = var.origin_type == "alb"
+  value     = var.origin_type == "alb" ? var.alb_domain_name : null
+  sensitive = false
 }
