0a1ba6a81be9c432d7847728ca912f23645a1506
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-05 13:01:23 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,19 @@
 variable "origin_type" {
-  description = "The type of origin for the CloudFront distribution (s3 or alb)."
+  description = "Type of origin (s3 or alb)"
   type        = string
 }
 
 variable "s3_bucket_name" {
-  description = "The name of the S3 bucket."
+  description = "Name of the S3 bucket"
   type        = string
-  default     = ""
 }
 
 variable "alb_domain_name" {
-  description = "The domain name of the ALB (used if `origin_type` is `alb`)."
+  description = "Domain name of the ALB"
   type        = string
-  default     = ""
 }
 
-variable "tags" {
-  default     = {}
-  description = "A map of tags to add to all resources"
-  type        = map(string)
+variable "acm_certificate_arn" {
+  description = "ARN of the ACM certificate for CloudFront"
+  type        = string
 }
