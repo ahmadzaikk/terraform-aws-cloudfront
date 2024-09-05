051b2d6501dcde5da142fe0e01ae9987fa15a49a
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-05 12:53:02 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,9 @@
-output "cloudfront_distribution_id" {
-  description = "The ID of the CloudFront distribution."
-  value       = aws_cloudfront_distribution.this.id
-}
-
-output "cloudfront_distribution_domain_name" {
-  description = "The domain name of the CloudFront distribution."
-  value       = aws_cloudfront_distribution.this.domain_name
-}
-
 output "s3_bucket_id" {
-  description = "The ID of the S3 bucket."
-  value       = aws_s3_bucket.origin.id
+  value     = aws_s3_bucket.this.bucket
+  condition = var.origin_type == "s3"
 }
 
-output "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket."
-  value       = aws_s3_bucket.origin.arn
+output "alb_domain_name" {
+  value     = var.alb_domain_name
+  condition = var.origin_type == "alb"
 }
