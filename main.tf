05448fce621e9c701f6b168bd1fa22f13839d9de
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-05 13:37:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "this" {
   count = var.origin_type == "s3" ? 1 : 0
-  tags  = var.tags
+  tag   = var.tags
   bucket = var.s3_bucket_name
 }
 
@@ -39,41 +39,28 @@ resource "aws_cloudfront_distribution" "this" {
     domain_name = var.origin_type == "s3" ? aws_s3_bucket.this[0].bucket_regional_domain_name : var.alb_domain_name
     origin_id   = var.origin_type == "s3" ? "S3-${aws_s3_bucket.this[0].bucket}" : "ALB-${var.alb_domain_name}"
 
-    dynamic "s3_origin_config" {
-      for_each = var.origin_type == "s3" ? [1] : []
-      content {
-        origin_access_identity = aws_cloudfront_origin_access_identity.this[0].cloudfront_access_identity_path
-      }
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.this[0].cloudfront_access_identity_path
     }
 
-    dynamic "custom_origin_config" {
-      for_each = var.origin_type == "alb" ? [1] : []
-      content {
-        origin_protocol_policy = "http-only" # or "https-only" based on your needs
-        http_port              = 80
-        https_port             = 443
-        origin_ssl_protocols   = ["TLSv1.2"]
-      }
+    custom_origin_config {
+      origin_protocol_policy = "http-only" # or "https-only" based on your needs
+      http_port              = 80
+      https_port             = 443
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 
   default_cache_behavior {
-    target_origin_id = var.origin_type == "s3" ? "S3-${aws_s3_bucket.this[0].bucket}" : "ALB-${var.alb_domain_name}"
+    target_origin_id       = var.origin_type == "s3" ? "S3-${aws_s3_bucket.this[0].bucket}" : "ALB-${var.alb_domain_name}"
     viewer_protocol_policy = "allow-all"
 
-    allowed_methods {
-      items = ["GET", "HEAD"]
-      quantity = 2
-    }
-
-    cached_methods {
-      items = ["GET", "HEAD"]
-      quantity = 2
-    }
+    # These are arguments, not blocks
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
 
     forwarded_values {
       query_string = false
-      headers      = ["Host"]
       cookies {
         forward = "none"
       }
