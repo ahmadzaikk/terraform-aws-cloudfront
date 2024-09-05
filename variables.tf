115ac8bb31fa0ba1fc524cdfcb479e6e186d9a01
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-05 12:57:01 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,3 +14,9 @@ variable "alb_domain_name" {
   type        = string
   default     = ""
 }
+
+variable "tags" {
+  default     = {}
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+}
