e6f96269e32c5681aa69a72b531e4f9c04d003be
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-05 14:53:15 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_cloudfront_distribution" "this" {
     origin_id   = "S3-${aws_s3_bucket.this.bucket}"
 
     s3_origin_config {
-      origin_access_control_id = aws_cloudfront_origin_access_control.this.id  # Correct usage of OAC
+      origin_access_control_id = aws_cloudfront_origin_access_control.this.id  # Ensure this is correctly used
     }
   }
 
