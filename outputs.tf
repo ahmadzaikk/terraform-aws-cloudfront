334787f97a690f76c2e733bdcfe358310774a37e
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 14:48:38 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "cloudfront_domain_name" {
-  value = var.origin_type == "s3" ? aws_cloudfront_distribution.s3[0].domain_name : aws_cloudfront_distribution.alb[0].domain_name
+# Existing resources...
+
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket (only when origin_type is 's3')"
+  value       = var.origin_type == "s3" ? aws_s3_bucket.this[0].bucket : null
+  condition   = var.origin_type == "s3"
 }
