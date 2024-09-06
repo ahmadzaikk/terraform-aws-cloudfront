c0fe8aa7740097887553fe3c0f31a83842be7d77
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 08:57:57 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
+# Outputs
 output "s3_bucket_id" {
   value = var.origin_type == "s3" ? aws_s3_bucket.this[0].bucket : null
   description = "The ID of the S3 bucket, if the origin is S3."
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.this.id
+  description = "The ID of the CloudFront distribution."
 }
