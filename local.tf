353d690ac1d9ca40fe00ae15ee2327ab281c1322
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 11:43:38 -0700

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,1 +1,6 @@
 
+locals {
+  cache_policy_id = var.cache_policy_type == "cache-optimized" ?
+    data.aws_cloudfront_cache_policy.cache_optimized.id :
+    data.aws_cloudfront_cache_policy.caching_disabled.id
+}
