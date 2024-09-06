1f08db22a62f20042097b8af7cac88123e2287db
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 15:07:43 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,8 +5,6 @@ output "s3_bucket_name" {
   value       = var.origin_type == "s3" ? aws_s3_bucket.this[0].bucket : null
 
 }
-
-
 # Output for CloudFront distribution (S3)
 output "cloudfront_s3_domain_name" {
   value       = var.origin_type == "s3" && length(aws_cloudfront_distribution.s3) > 0 ? aws_cloudfront_distribution.s3[0].domain_name : null
