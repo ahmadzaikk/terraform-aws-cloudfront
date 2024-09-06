4a65ff158fc542989f40b0fb7a35543241be3553
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 11:44:23 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,13 +54,9 @@ resource "aws_cloudfront_distribution" "s3" {
     target_origin_id       = "S3-${aws_s3_bucket.this[0].bucket}"
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
-
-    cache_policy_id = var.cache_policy_type == "cache-optimized" ?
-      data.aws_cloudfront_cache_policy.cache_optimized.id :
-      data.aws_cloudfront_cache_policy.caching_disabled.id
-
-    allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
-    cached_methods  = ["GET", "HEAD"]
+    cache_policy_id        = local.cache_policy_id
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
   }
 
   restrictions {
@@ -95,13 +91,9 @@ resource "aws_cloudfront_distribution" "alb" {
     target_origin_id       = "ALB-${var.alb_arn}"
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
-
-    cache_policy_id = var.cache_policy_type == "cache-optimized" ?
-      data.aws_cloudfront_cache_policy.cache_optimized.id :
-      data.aws_cloudfront_cache_policy.caching_disabled.id
-
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods  = ["GET", "HEAD"]
+    cache_policy_id        = local.cache_policy_id
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
   }
 
   restrictions {
