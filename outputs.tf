97a4be6d908e219f9698088b4dc8a808a8b9097e
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 09:27:04 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,20 @@
-# Outputs
-output "s3_bucket_id" {
-  value = var.origin_type == "s3" ? aws_s3_bucket.this[0].bucket : null
-  description = "The ID of the S3 bucket, if the origin is S3."
+variable "origin_type" {
+  description = "The type of origin for the CloudFront distribution. Can be 's3' or 'alb'."
+  type        = string
+  default     = "s3" # Change to "alb" to use ALB as origin
 }
 
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.this.id
-  description = "The ID of the CloudFront distribution."
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket."
+  type        = string
+}
+
+variable "alb_arn" {
+  description = "The ARN of the Application Load Balancer."
+  type        = string
+}
+
+variable "default_root_object" {
+  description = "The default root object for the CloudFront distribution."
+  type        = string
 }
