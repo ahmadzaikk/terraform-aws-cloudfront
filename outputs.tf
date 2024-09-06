ef7202d1e1325f7086c787bb38bc69a30ef5e4fe
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 10:42:50 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "cloudfront_domain_name" {
 }
 
 output "s3_bucket_name" {
-  value = var.origin_type == "s3" ? aws_s3_bucket.example.bucket : null
+  value = var.origin_type == "s3" ? aws_s3_bucket.this.bucket : null
 }
