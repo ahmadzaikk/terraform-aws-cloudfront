4bb35645fc3a60ef30a35530ef8d0a237b666399
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 10:48:03 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,9 @@ resource "aws_cloudfront_distribution" "this" {
     domain_name = var.origin_type == "s3" ? aws_s3_bucket.this[0].bucket_regional_domain_name : var.alb_arn
     origin_id   = var.origin_type == "s3" ? "S3-${aws_s3_bucket.this[0].bucket}" : "ALB-${var.alb_arn}"
 
-    origin_access_control_id = var.origin_type == "s3" ? aws_cloudfront_origin_access_control.this[0].id : null
+    s3_origin_config {
+      origin_access_identity = var.origin_type == "s3" ? aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path : null
+    }
 
     custom_origin_config {
       http_port              = 80
@@ -42,6 +44,7 @@ resource "aws_cloudfront_distribution" "this" {
     cloudfront_default_certificate = true
   }
 }
+
 
 
 resource "aws_cloudfront_origin_access_control" "this" {
