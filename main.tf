e9119993ddf2d54c6c75f71c8934f99231c8a797
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 10:01:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,17 @@
-# Define S3 Bucket
 resource "aws_s3_bucket" "this" {
-  count = var.origin_type == "s3" ? 1 : 0
   bucket = var.s3_bucket_name
   tags   = var.tags
 }
-
-# Define CloudFront Origin Access Control (OAC) for S3
-resource "aws_cloudfront_origin_access_control" "this" {
-  count                             = var.origin_type == "s3" ? 1 : 0
-  name                              = "${var.s3_bucket_name}-oac"
-  description                       = "OAC for S3 bucket ${var.s3_bucket_name}"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-data "aws_cloudfront_cache_policy" "cache-optimized" {
-  name = "Managed-CachingOptimized"
-}
-## cloudfront distribution
 
 resource "aws_cloudfront_distribution" "this" {
   enabled             = true
   default_root_object = var.default_root_object
 
-  # Define the origin block conditionally
   origin {
     domain_name = var.origin_type == "s3" ? aws_s3_bucket.this[0].bucket_regional_domain_name : var.alb_arn
     origin_id   = var.origin_type == "s3" ? "S3-${aws_s3_bucket.this[0].bucket}" : "ALB-${var.alb_arn}"
 
-    # Set origin_access_control_id only if origin type is S3
+    # For S3 origin, ensure that the S3 bucket is properly referenced
     origin_access_control_id = var.origin_type == "s3" ? aws_cloudfront_origin_access_control.this[0].id : null
   }
 
@@ -59,35 +42,3 @@ resource "aws_cloudfront_distribution" "this" {
     cloudfront_default_certificate = true
   }
 }
-
-
-
-
-
-# Define S3 Bucket Policy
-resource "aws_s3_bucket_policy" "this" {
-  count  = var.origin_type == "s3" ? 1 : 0
-  bucket = aws_s3_bucket.this[0].id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.this[0].arn}/*"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.this.id}"
-          }
-        }
-      }
-    ]
-  })
-}
-
-# Data for current AWS caller identity
-data "aws_caller_identity" "current" {}
