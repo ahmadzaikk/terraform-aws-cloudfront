a9701a3a777b9b694db61d5f054ccb177edc7d5f
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 10:39:32 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.this.domain_name
+}
 
-
-
+output "s3_bucket_name" {
+  value = aws_s3_bucket.this[0].bucket
+  condition = var.origin_type == "s3"
+}
