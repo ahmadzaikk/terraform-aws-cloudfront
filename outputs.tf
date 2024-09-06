8240aa2e29289af8921e5c9b52ec263f9add5a1e
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 08:51:50 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "s3_bucket_id" {
-  value = aws_s3_bucket.this[0].bucket
+  value = var.origin_type == "s3" ? aws_s3_bucket.this[0].bucket : null
+  description = "The ID of the S3 bucket, if the origin is S3."
 }
