955daf208ce41d4a54794cd50db0a5de0df84057
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 10:58:39 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,39 +4,31 @@ resource "aws_s3_bucket" "this" {
   tags   = var.tags
 }
 
-resource "aws_cloudfront_origin_access_identity" "this" {
-  comment = "OAI for S3 bucket ${var.s3_bucket_name}"
+resource "aws_cloudfront_origin_access_control" "this" {
+  count                            = var.origin_type == "s3" ? 1 : 0
+  name                             = "${var.s3_bucket_name}-oac"
+  description                      = "OAC for S3 bucket ${var.s3_bucket_name}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                 = "always"
+  signing_protocol                 = "sigv4"
 }
 
-resource "aws_cloudfront_distribution" "this" {
+resource "aws_cloudfront_distribution" "s3" {
+  count               = var.origin_type == "s3" ? 1 : 0
   enabled             = true
   default_root_object = var.default_root_object
 
   origin {
-    count       = var.origin_type == "s3" ? 1 : 0
     domain_name = aws_s3_bucket.this[0].bucket_regional_domain_name
     origin_id   = "S3-${aws_s3_bucket.this[0].bucket}"
 
     s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
-    }
-  }
-
-  origin {
-    count       = var.origin_type != "s3" ? 1 : 0
-    domain_name = var.alb_arn
-    origin_id   = "ALB-${var.alb_arn}"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_access_control_id = aws_cloudfront_origin_access_control.this[0].id
     }
   }
 
   default_cache_behavior {
-    target_origin_id       = var.origin_type == "s3" ? "S3-${aws_s3_bucket.this[0].bucket}" : "ALB-${var.alb_arn}"
+    target_origin_id       = "S3-${aws_s3_bucket.this[0].bucket}"
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
     cache_policy_id        = data.aws_cloudfront_cache_policy.cache-optimized.id
@@ -55,18 +47,42 @@ resource "aws_cloudfront_distribution" "this" {
   }
 }
 
+resource "aws_cloudfront_distribution" "alb" {
+  count               = var.origin_type != "s3" ? 1 : 0
+  enabled             = true
+  default_root_object = var.default_root_object
 
+  origin {
+    domain_name = var.alb_arn
+    origin_id   = "ALB-${var.alb_arn}"
 
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
 
-resource "aws_cloudfront_origin_access_control" "this" {
-  count                            = var.origin_type == "s3" ? 1 : 0
-  name                             = "${var.s3_bucket_name}-oac"
-  description                      = "OAC for S3 bucket ${var.s3_bucket_name}"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                 = "always"
-  signing_protocol                 = "sigv4"
+  default_cache_behavior {
+    target_origin_id       = "ALB-${var.alb_arn}"
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    cache_policy_id        = data.aws_cloudfront_cache_policy.cache-optimized.id
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
-
 
 data "aws_cloudfront_cache_policy" "cache-optimized" {
   name = "Managed-CachingOptimized"
