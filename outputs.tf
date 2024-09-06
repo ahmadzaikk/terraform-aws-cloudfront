6b4367395191cdee077af56b42e6cb66ef9a88b0
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 14:52:56 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 output "s3_bucket_name" {
   description = "The name of the S3 bucket (only when origin_type is 's3')"
   value       = var.origin_type == "s3" ? aws_s3_bucket.this[0].bucket : null
-  condition   = var.origin_type == "s3"
+
 }
 
 # Output for CloudFront distribution (S3)
