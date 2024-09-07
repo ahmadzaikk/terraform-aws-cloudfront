b95b832b9ca0d3b77eb835bb8f3b966320e7e4eb
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-09-06 17:03:47 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
-# Define an S3 bucket if the origin type is "s3"
 resource "aws_s3_bucket" "this" {
   count  = var.origin_type == "s3" ? 1 : 0
   bucket = var.s3_bucket_name
   tags   = var.tags
 }
 
-# Define an S3 bucket policy to allow CloudFront to access the bucket
 resource "aws_s3_bucket_policy" "this" {
-  count = var.origin_type == "s3" ? 1 : 0
+  count  = var.origin_type == "s3" ? 1 : 0
   bucket = aws_s3_bucket.this[0].id
 
   policy = jsonencode({
@@ -30,10 +28,8 @@ resource "aws_s3_bucket_policy" "this" {
   })
 }
 
-# Data source to get the current AWS account ID
 data "aws_caller_identity" "current" {}
 
-# Define CloudFront Origin Access Control (OAC) for S3 bucket
 resource "aws_cloudfront_origin_access_control" "this" {
   count                            = var.origin_type == "s3" ? 1 : 0
   name                             = "${var.s3_bucket_name}-oac"
@@ -43,20 +39,17 @@ resource "aws_cloudfront_origin_access_control" "this" {
   signing_protocol                 = "sigv4"
 }
 
-# Define CloudFront distribution for S3 origin
 resource "aws_cloudfront_distribution" "s3" {
   count               = var.origin_type == "s3" ? 1 : 0
   enabled             = true
   default_root_object = var.default_root_object
 
-  # Define the origin pointing to the S3 bucket
   origin {
     domain_name = aws_s3_bucket.this[0].bucket_regional_domain_name
     origin_id   = "S3-${aws_s3_bucket.this[0].bucket}"
     origin_access_control_id = aws_cloudfront_origin_access_control.this[0].id
   }
 
-  # Define default cache behavior for the distribution
   default_cache_behavior {
     target_origin_id       = "S3-${aws_s3_bucket.this[0].bucket}"
     viewer_protocol_policy = "redirect-to-https"
@@ -66,7 +59,19 @@ resource "aws_cloudfront_distribution" "s3" {
     cached_methods         = ["GET", "HEAD"]
   }
 
-  # Configure custom error responses based on provided values
+  viewer_certificate {
+    acm_certificate_arn            = var.acm_certificate_arn != "" ? var.acm_certificate_arn : null
+    cloudfront_default_certificate = var.acm_certificate_arn == "" ? true : false
+    ssl_support_method             = var.acm_certificate_arn != "" ? "sni-only" : null
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
   dynamic "custom_error_response" {
     for_each = var.error_pages != null ? var.error_pages : {}
     content {
@@ -76,27 +81,13 @@ resource "aws_cloudfront_distribution" "s3" {
       error_caching_min_ttl = custom_error_response.value.error_caching_min_ttl
     }
   }
-
-  # No geographic restrictions
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  # Use default CloudFront certificate
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
 }
 
-# Define CloudFront distribution for ALB origin
 resource "aws_cloudfront_distribution" "alb" {
   count               = var.origin_type != "s3" ? 1 : 0
   enabled             = true
   default_root_object = var.default_root_object
 
-  # Define the origin pointing to the ALB
   origin {
     domain_name = var.alb_arn
     origin_id   = "ALB-${var.alb_arn}"
@@ -109,7 +100,6 @@ resource "aws_cloudfront_distribution" "alb" {
     }
   }
 
-  # Define default cache behavior for the distribution
   default_cache_behavior {
     target_origin_id       = "ALB-${var.alb_arn}"
     viewer_protocol_policy = "redirect-to-https"
@@ -119,7 +109,19 @@ resource "aws_cloudfront_distribution" "alb" {
     cached_methods         = ["GET", "HEAD"]
   }
 
-  # Configure custom error responses based on provided values
+  viewer_certificate {
+    acm_certificate_arn            = var.acm_certificate_arn != "" ? var.acm_certificate_arn : null
+    cloudfront_default_certificate = var.acm_certificate_arn == "" ? true : false
+    ssl_support_method             = var.acm_certificate_arn != "" ? "sni-only" : null
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
   dynamic "custom_error_response" {
     for_each = var.error_pages != null ? var.error_pages : {}
     content {
@@ -128,20 +130,5 @@ resource "aws_cloudfront_distribution" "alb" {
       response_code         = custom_error_response.value.response_code
       error_caching_min_ttl = custom_error_response.value.error_caching_min_ttl
     }
-  }
-
-  # No geographic restrictions
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  # Use default CloudFront certificate
-   viewer_certificate {
-    acm_certificate_arn            = var.acm_certificate_arn
-    ssl_support_method             = "sni-only"
-    minimum_protocol_version       = "TLSv1.2_2021"
-    cloudfront_default_certificate = false
   }
 }
